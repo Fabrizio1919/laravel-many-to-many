31e4abc2ba8bb4e7b000178803801189f7a5ff88
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-06-06 12:40:11 +0200

    Fix structure in project

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,30 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
 
+    // ROTTA CHE PORTA ALLA VISUALIZZAZIONE DELLA DASHBOARD (GESTITA DA UN CONTROLLER)
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+    Route::resource('projects', AdminProjectController::class);
 
-/* TODO: create Dashboard Controller */
-
-
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
-    // responds to url /admin
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); // admin.dashboard
-    Route::resource('posts', PostController::class)->parameters([
-        'posts' => 'post:slug'
-    ]);
-});
-
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
